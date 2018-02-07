31d5a416432893c198a4b93e63b941057c441a81
nxlai <nxlai@i9i8.com>
2018-02-07 14:39:37 +0800

    测试

--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -79,15 +79,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    msb 点下面
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://www.liepin.com">涨工资</a>
                 </div>
             </div>
         </div>
